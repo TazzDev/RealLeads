c97eb7cabdabf75bb9a707b623f676c4df7360f7
Thomson <thomsonbingo@gmail.com>
2020-08-30 15:52:12 +0530

    barebones

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname,'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname,'public')
+    }
+}
